a908d8eefdeb10820a6a0e650dfccbfab0f0d47f
santiagoavacen25-sys <santiagoavacen25@gmail.com>
2026-08-11 12:08:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title("Mi primera pruebe manual")
 
 nombre= st.text_input("Escribe tu juego favorito:")
+plataforma = st.text_input("Escribe tu plataforma:")
 
 if st.button("Mostrar juego"):
     st.write(f"Tu juego favorito es: {nombre}")
