631781b6a4dd424f8c032ec8c5bdfc1327da6069
santiagoavacen25-sys <santiagoavacen25@gmail.com>
2026-08-12 11:21:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.title("Mi primera pruebe manual")
+st.title("Mi primera prueba manual")
 
-nombre= st.text_input("Escribe tu juego favorito:")
-plataforma = st.text_input("Escribe tu plataforma:")
+nombre = st.text_input("Escribe tu juego favorito:")
+# Cambiamos st.text_input por st.selectbox
+plataforma = st.selectbox("Selecciona tu plataforma:", ["PC", "PlayStation 5", "Xbox", "Nintendo Switch"])
 
 if st.button("Mostrar juego"):
     st.write(f"Juegas {nombre} en {plataforma}")
