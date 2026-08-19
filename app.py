0c8c6dd88f2fe927fc4de4e4fc512efe11de869c
santiagoavacen25-sys <santiagoavacen25@gmail.com>
2026-08-18 23:12:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if prompt := st.chat_input("Escribe una pregunta para el asistente..."):
             mensajes_for_api = [{"role": "system", "content": CONTEXTO_ASISTENTE}] + st.session_state.historial_chat
             
             completion = client.chat.completions.create(
-                model="llama3-8b-8192",model="llama-3.3-70b-versatile",
+                model="llama-3.3-70b-versatile",
                 messages=mensajes_for_api
             )
             
