795b6186ce828957b2505478373e3aece498be40
santiagoavacen25-sys <santiagoavacen25@gmail.com>
2026-08-20 23:55:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ section[data-testid="stSidebar"] {
 
 header[data-testid="stHeader"]{
     background: transparent !important;
-    background-color: rgba(255,255,255,0.07 !important;    
+    background-color: rgba(18, 20, 26 0.2) !important;    
     
 }
 
