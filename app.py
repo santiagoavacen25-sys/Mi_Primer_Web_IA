f57b4c9b0e9d838d786bcb7351d51959ec0918a2
santiagoavacen25-sys <santiagoavacen25@gmail.com>
2026-08-21 14:07:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ except Exception:
 # SESSION STATE
 # =========================================================
 if "model" not in st.session_state:
-    st.session_state.model = "llama-3.1-8b-instant"
+    st.session_state.model = "llama3-70b-8192"
     
 if "chats" not in st.session_state:
     st.session_state.chats = {} 
