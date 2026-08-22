a366cd451863b4a8e5c3c0fca4e3403d1d07bc13
santiagoavacen25-sys <santiagoavacen25@gmail.com>
2026-08-21 23:21:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,10 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 # MODELO ACTIVO EN GROQ
-st.session_state.model = "llama-3.3-70b-versatile"
+# El más rápido y estable):
+st.session_state.model = "llama-3.1-8b-instant"
+
+
 
 # =========================================================
 # LOGO Y CABECERA
